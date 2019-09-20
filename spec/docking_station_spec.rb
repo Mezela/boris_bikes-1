98c2b8f1abe4b0bf42c49f56241bb9a8aacefe75
Mezela <p_mezue@hotmail.com>
2019-09-20 16:08:50 +0100

    Use double and method stubs to isolate unit tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,9 +4,12 @@ describe DockingStation do
     # it { is_expected.to respond_to(:release_bike) }
     # it { is_expected.to respond_to(:bikes) }
     # it { is_expected.to respond_to(:dock).with(1).argument }
-
+    let(:bike) {double :bike}
+    
     it 'releases a working bike' do
-        bike = Bike.new
+        allow(bike).to receive(:working?).and_return(true)
+        # shorthand version of method stub
+        # bike = double("bike", :working? => true)
         subject.dock(bike)
         expect(subject.release_bike).to be_working
     end
@@ -18,8 +21,8 @@ describe DockingStation do
     
         it 'has variable capacity based on user input' do
           station = DockingStation.new(35)
-          35.times{station.dock(Bike.new)}
-          expect{station.dock(Bike.new)}.to raise_error("Docking station is full")
+          35.times{station.dock(double(:bike))}
+          expect{station.dock(double(:bike))}.to raise_error("Docking station is full")
         end
       end
 
@@ -27,34 +30,32 @@ describe DockingStation do
     describe '#dock' do
         
         it 'docks something' do
-            my_bike = Bike.new
+            allow(bike).to receive(:working?).and_return(true)
             station = DockingStation.new
-            station.dock(my_bike)
-            expect(station.release_bike).to eq my_bike
+            station.dock(bike)
+            expect(station.release_bike).to eq bike
         end
 
         it ' raises error if dock is full' do
             # setup
             station = DockingStation.new
             num = DockingStation::DEFAULT_CAPACITY
-            num.times{station.dock(Bike.new)}
-            my_bike = Bike.new
-            
-           expect{station.dock(my_bike)}.to raise_error("Docking station is full")
+            num.times{station.dock(double(:bike))}
+           expect{station.dock(bike)}.to raise_error("Docking station is full")
         end
     end
 
     describe '#release_bike' do
         it 'release a bike' do
+            allow(bike).to receive(:working?).and_return(true)
             # setup
-            my_bike = Bike.new
-            subject.dock(my_bike)
+            subject.dock(bike)
 
             # execute
             released_bike = subject.release_bike
 
             # assert
-            expect(released_bike).to eq my_bike
+            expect(released_bike).to eq bike
         end
 
         it 'raise error if no bike docked' do
@@ -62,14 +63,15 @@ describe DockingStation do
         end
 
         it "doesn't release a broken bike" do
-            bike = Bike.new
+            allow(bike).to receive(:working?).and_return(false)
+            allow(bike).to receive(:report_broken).and_return(true)
             bike.report_broken
             subject.dock(bike)
             expect{subject.release_bike}.to raise_error("No available working bikes")
         end
 
         it 'docks a bike whether its broken or not' do
-            bike = Bike.new
+            allow(bike).to receive(:report_broken).and_return(true)
             bike.report_broken
             expect(subject.dock(bike)).to eq [bike]
         end
